d6ab32ce484321c0823959435d3f6d71eee3e2a6
camilo medina <you@example.com>
2019-10-24 11:09:06 -0500

    master Camilo Medina spring base line

---
+++ MobEu-Hiring-Java/src/main/java/com/mobiquityinc/MobEuHiringJava/MobEuHiringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mobiquityinc.MobEuHiringJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobEuHiringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobEuHiringJavaApplication.class, args);
+	}
+
+}
---
+++ MobEu-Hiring-Java/src/test/java/com/mobiquityinc/MobEuHiringJava/MobEuHiringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiquityinc.MobEuHiringJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobEuHiringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
