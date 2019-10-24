a23e349ee72878c53b1abe4deb93be6f06f720e4
camilo medina <you@example.com>
2019-10-24 14:23:08 -0500

    master Camilo Medina add test for read the file and compare with a string

--- MobEu-Hiring-Java/src/main/java/com/mobiquityinc/packer/Packer.java
+++ MobEu-Hiring-Java/src/main/java/com/mobiquityinc/packer/Packer.java
@@ -2,12 +2,32 @@ package com.mobiquityinc.packer;
 
 import com.mobiquityinc.exception.APIException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Packer {
 
-  private Packer() {
-  }
+	public Packer() {
+	}
 
-  public static String pack(String filePath) throws APIException {
-    return null;
-  }
+	public static String pack(String filePath) throws APIException {
+	  Path path;
+      String data;
+      Stream<String> lines = null;
+		try {
+			path = Paths.get(Packer.class.getClassLoader().getResource(filePath).toURI());
+			lines = Files.lines(path);
+			data = lines.collect(Collectors.joining("\n"));
+		} catch (URISyntaxException | IOException e) {
+			throw new APIException(e.getMessage(), e);
+		} finally {
+          lines.close();
+        }
+		return data;
+	}
 }
